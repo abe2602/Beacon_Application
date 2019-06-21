2f1a7c90326096ed6d6cd1a0613ba017d2f4f023
Bruno Bacelar Abe <messengerabe@hotmail.com>
2019-06-21 15:47:04 -0300

    [1] RxJava2 Beacon added
    1.  Encontra os beacons cadastrados.
    2. Encontra os dados do beacon cadastrado e mostra no log;
    3. Cálculo da distância ainda está bugado.

--- README.md
+++
@@ -1,1 +0,0 @@
-# Beacon_Application
--- app/src/main/java/com/example/beacon/MainActivity.java
+++ app/src/main/java/com/example/beacon/MainActivity.java
@@ -1,59 +1,159 @@
 package com.example.beacon;
 
-
-import android.os.RemoteException;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import org.altbeacon.beacon.Beacon;
-import org.altbeacon.beacon.BeaconConsumer;
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.MonitorNotifier;
-import org.altbeacon.beacon.RangeNotifier;
-import org.altbeacon.beacon.Region;
+import com.github.pwittchen.reactivebeacons.library.rx2.ReactiveBeacons;
 
-import java.util.Collection;
+import java.util.ArrayList;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
-public class MainActivity extends AppCompatActivity implements BeaconConsumer {
-    protected static final String TAG = "HelpMe";
-    private BeaconManager beaconManager;
+/**
+ * Eu adicionei o MAC dos MEUS beacons aqui. Rodrigo, caso você vá testar
+ * não esqueça de modificar pra os seus beacons;
+ * */
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "HelpMe";
+    private String TAGERROR = "ErrorHelp";
+    private ArrayList<String> knownBeacons = new ArrayList<>();
+    private ReactiveBeacons reactiveBeacons;
+    private Disposable subscription;
+    private ArrayList<BeaconHelper> meanRssi = new ArrayList<>();
+    private int count = 0;
+
+    //Me ajuda a fazer a média
+    class BeaconHelper{
+        private String beaconName;
+
+        public ArrayList<Double> getDist() {
+            return dist;
+        }
+
+        public void setDist(ArrayList<Double> dist) {
+            this.dist = dist;
+        }
+
+        private ArrayList<Double> dist = new ArrayList<>();
+
+        public String getBeaconName() {
+            return beaconName;
+        }
+
+        public void setBeaconName(String beaconName) {
+            this.beaconName = beaconName;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        beaconManager = BeaconManager.getInstanceForApplication(this);
-        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
-        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
-         beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-20v"));
-        beaconManager.bind(this);
-    }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        beaconManager.unbind(this);
-    }
-    @Override
-    public void onBeaconServiceConnect() {
-        Log.d(TAG, "ENTREI");
-        beaconManager.removeAllRangeNotifiers();
-        beaconManager.addRangeNotifier(new RangeNotifier() {
-            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                if (beacons.size() > 0) {
-                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
-                }else{
-                    Log.d(TAG, "Vazio");
-                }
-            }
-        });
 
-        try {
-            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {  Log.d(TAG, e.toString());  }
+        //Beacon conhecido
+        this.knownBeacons.add("0C:F3:EE:54:2F:C6");
+        BeaconHelper bh = new BeaconHelper();
+        bh.setBeaconName("0C:F3:EE:54:2F:C6");
+
+        this.meanRssi.add(0, bh);
+
+        reactiveBeacons = new ReactiveBeacons(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Caso não suporte BLE
+        if (!reactiveBeacons.isBleSupported()) {
+            Log.d(TAG, "Não suportado");
+            return;
+        }
+
+        // Checa se tem as permissões necessárias
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAGERROR, "Não há permissões");
+            return;
+        }
+
+        //Observable!
+        subscription = reactiveBeacons.observe()
+                .subscribeOn(Schedulers.computation()) //Faz o trabalho numa thread separada
+                .observeOn(AndroidSchedulers.mainThread()) //Observa na thread principal
+                .subscribe(beacon-> {
+
+                    /*
+                     * txPower = MeasuredPower: RSSI esperado na distancia de 1 metro
+                     * RSSI = Received Signal Strength Indicator: Força do sinal do beacon
+                     * N: constante do ambiente, varia de 2 - 4
+                     * */
+                    for (String myBeacon: knownBeacons) {
+                        if(beacon.macAddress.address.equals(myBeacon)){
+
+                            Log.d(TAG, "OBSERVANDO o beacon: " + myBeacon);
+
+                            //Transforma RSSI em Metros (BUGANDOOOOOOOOOOOOOO)
+                            double ratio = beacon.rssi*1.0/beacon.txPower;
+                            ratio = ratio/(10 * 3.0);
+                            double dist = Math.pow(10, ratio);
+
+                            Log.d(TAG, "Distancia: " + (dist));
+
+                            for (BeaconHelper bh: meanRssi){
+                                if(bh.getBeaconName().equals(myBeacon)){
+                                    bh.getDist().add(dist);
+                                }
+                            }
+
+                            //Faz a média de 5 medições
+                            this.count++;
+
+                            if(count == 5){
+                                double auxDist = 0;
+                                int sizeList = 0;
+
+                                for(BeaconHelper bh: meanRssi){
+                                    if(bh.getBeaconName().equals(myBeacon)){
+                                        sizeList = bh.getDist().size();
+                                        for (double sum: bh.getDist()){
+                                            auxDist += sum;
+                                        }
+                                    }
+                                }
+                                count = 0;
+                                double finalDist = (auxDist/sizeList);
+
+                                Log.d(TAG, "Distancia Final: " + (finalDist));
+                            }
+                        }else{
+                            Log.d(TAGERROR, "Não eh o meu!");
+                        }
+                    }
+
+                }, error ->{
+                    Log.d(TAGERROR, "Erro no subscribe");
+                });
+    }
+
+    //Lida com os observables quando não estão mais ativos
+    @Override protected void onPause() {
+        super.onPause();
+        if (subscription != null && !subscription.isDisposed()) {
+            subscription.dispose();
+        }
+    }
+
+    //Lida com os observables quando são destruídos
+    @Override protected void onDestroy(){
+        super.onDestroy();
+        subscription.dispose();
     }
 }
