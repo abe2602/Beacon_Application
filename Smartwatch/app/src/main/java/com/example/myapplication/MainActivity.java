de00f05ba13bd3f9b29af608d65baa8749b52761
Bruno Bacelar Abe <messengerabe@hotmail.com>
2019-08-27 22:41:14 -0300

    [Final-Version] Smartwatch atualizado

---
+++ Smartwatch/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.support.wearable.activity.WearableActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.wearable.MessageApi;
+import com.patloew.rxwear.RxWear;
+import com.patloew.rxwear.transformers.MessageEventGetDataMap;
+
+public class MainActivity extends WearableActivity {
+    private RxWear rxWear;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Enables Always-on
+        setAmbientEnabled();
+
+        //Recupe o nome do device
+        String deviceName = Settings.Secure.getString(getContentResolver(), "bluetooth_name");
+
+        rxWear = new RxWear(this);
+        rxWear.message().listen("/" + deviceName, MessageApi.FILTER_LITERAL)
+                .compose(MessageEventGetDataMap.noFilter())
+                .doOnNext(item ->{
+                    Log.d("HelpMe", "buga buga");
+                })
+                .subscribe(bom -> {
+                    Log.d("HelpMe", bom.toString());
+                }, ruim ->{
+                    Log.d("HelpMe", ruim.toString());
+                });
+
+        Log.d("HelpMe", "uga ugaa");
+    }
+}
